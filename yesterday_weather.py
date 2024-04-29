8895bea65effdb715993635a810d218606466dfd
jaehyunkim0210 <103502575+jaehyunkim0210@users.noreply.github.com>
2024-04-28 23:01:41 -0400

    Update yesterday_weather.py

--- yesterday_weather.py
+++ yesterday_weather.py
@@ -1,12 +1,20 @@
 import requests
-from datetime import datetime, timedelta
 
-def fetch_yesterday_weather(api_key, latitude, longitude):
-    #Fetch yesterday's weather data using the OpenWeatherMap one call timemachine API.
-    yesterday = datetime.now() - timedelta(days=1)
-    yesterday_timestamp = int(yesterday.timestamp())
-    yesterday_url = f"http://api.openweathermap.org/data/2.5/onecall/timemachine?lat={latitude}&lon={longitude}&dt={yesterday_timestamp}&appid={api_key}&units=metric"
+def fetch_yesterday_weather(api_key, latitude, longitude,todaytime):
+    # Fetch yesterday's weather data using the OpenWeatherMap One Call Timemachine API.
+    # Used Unix time, making the yesterday_timestamp 86400 seconds away from current time. 
+    
+    yesterday_timestamp = todaytime -86400
+    yesterday_url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={latitude}&lon={longitude}&dt={yesterday_timestamp}&appid={api_key}"
     yesterday_response = requests.get(yesterday_url)
-    yesterday_data = yesterday_response.json()
-    yesterday_temp = yesterday_data['current']['temp']
-    return yesterday_temp
+    
+    if yesterday_response.status_code == 200:  # Check if the request was successful
+        yesterday_data = yesterday_response.json()
+        if 'current' in yesterday_data:
+            yesterday_temp = yesterday_data['current']['temp']
+            return yesterday_temp
+        else:
+            return "Yesterday weather data not found."
+    else:
+        return f"Failed to fetch data: {yesterday_response.status_code}"
+    
