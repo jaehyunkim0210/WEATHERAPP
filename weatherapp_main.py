f0e8e872ec25634306b9ab239323340223940f58
jaehyunkim0210 <103502575+jaehyunkim0210@users.noreply.github.com>
2024-04-28 23:01:24 -0400

    Update weatherapp_main.py

--- weatherapp_main.py
+++ weatherapp_main.py
@@ -6,40 +6,41 @@ from fetch_rainforecast import fetch_rainforecast
 from compare_temperature import compare_temperature
 from recommend_outfit import recommend_outfit 
 
-
 layout = [
-    [sg.Text("Weather Information", font=('Helvetica', 16))],
-    # [sg.Text("Location(City): "), sg.InputText(key = '-LOCATION-')],
-    [sg.Text(size=(40, 1), key='-WEATHER-')],
-    [sg.Text(size=(40, 1), key='-OUTFIT-')],
-    [sg.Text(size=(40, 1),key = '-RAIN-')],
+    [sg.Text("Weather Information", font=('Helvetica', 16), key = '-WEATHER-')],
+    [sg.Text("Location(City): "), sg.InputText(key = '-LOCATION-')],
+    [sg.Text('Recommended outfit for the day', key = "-OUTFIT-")],
     [sg.Button("Refresh"), sg.Button("Exit")]
 ]
 
-
 window = sg.Window("Weather App", layout)
+event, values = window.read()
+
+api_key = 'c5bcaa9e52187e78b61339a89aafdc3f'  
+city = values['-LOCATION-']
+
+today_temp,latitude,longitude,todaytime = today_weather(api_key, city)
+today_temp = float(today_temp)
+latitude = float(latitude)
+longitude = float(longitude)
+
+yesterday_temp =fetch_yesterday_weather(api_key,latitude,longitude,todaytime)
+yesterday_temp = float(yesterday_temp)
+comparison = compare_temperature(today_temp, yesterday_temp)
+outfit = recommend_outfit(today_temp)
+rainforecast = fetch_rainforecast(api_key,city)
+rainforecast = float(rainforecast)
+
+raining_dress = raining(rainforecast)
 
 while True:
-    event, values = window.read()
-
-    api_key = 'c5bcaa9e52187e78b61339a89aafdc3f'  
-    city = values['-LOCATION-']
-
-    today_temp,latitude,longitude = today_weather(api_key, city)
-    yesterday_temp =fetch_yesterday_weather(api_key,latitude,longitude)
-    comparison = compare_temperature(today_temp, yesterday_temp)
-    outfit = recommend_outfit(today_temp)
-    rainforecast = fetch_rainforecast(api_key,city)
-    raining_dress = raining(rainforecast)
-    
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
     elif event == 'Refresh':
-        rain_text = f"Rain/Snow forecast for Today: {raining_dress}."
         weather_text = f"Today's temperature: {today_temp}°C, {comparison} than yesterday." 
-        window['-RAIN-'].update(rain_text)
+        rain_text = f"Rain/Snow forecast for Today: {raining_dress}."
         window['-WEATHER-'].update(weather_text)
+        window['-RAIN-'].update(rain_text)
         window['-OUTFIT-'].update(f"Recommended outfit: {outfit}")
-        
 window.close()
 
